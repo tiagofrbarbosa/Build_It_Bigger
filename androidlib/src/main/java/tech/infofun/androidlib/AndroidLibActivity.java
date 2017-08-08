9c4fba5fb584d916d54250784fb85a2124063caf
Tiago Freire Barbosa <tfreirebar@gmail.com>
2017-08-08 14:53:17 -0300

    feat: adding avatars in android lib to leave more fun

--- androidlib/src/main/java/tech/infofun/androidlib/AndroidLibActivity.java
+++ androidlib/src/main/java/tech/infofun/androidlib/AndroidLibActivity.java
@@ -3,13 +3,20 @@ package tech.infofun.androidlib;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.Random;
 
 
 public class AndroidLibActivity extends AppCompatActivity {
 
     public final static String EXTRA_JOKE = "EXTRA_JOKE";
     private TextView myText;
+    private ImageView avatar;
+    private int id_avatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,14 @@ public class AndroidLibActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String joke = intent.getStringExtra(EXTRA_JOKE);
+
+        avatar = (ImageView) findViewById(R.id.iv_avatar);
+
+        id_avatar = new Random().nextInt();
+
+        Picasso.with(this)
+                .load("https://api.adorable.io/avatars/285/" + id_avatar + ".png")
+                .into(avatar);
 
         myText = (TextView) findViewById(R.id.myText);
         myText.setText(joke);
